1cde4c0a12c65ced0b1989918b4b0fe5736ed143
Venkat Reddy <103410334+venkatreddybandi1999@users.noreply.github.com>
2023-09-25 11:02:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $email=$_POST['emailid'];
 $mobileno=$_POST['mobileno'];
 $dscrption=$_POST['description'];
 $query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
-echo "<script>alert('Your information succesfully submitted');</script>";
+echo "<script>alert('Your information is succesfully submitted');</script>";
 echo "<script>window.location.href ='index.php'</script>";
 
 } ?>
